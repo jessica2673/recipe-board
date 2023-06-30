487a4938d0321e9db4d5320a878795b1c095f012
Jessica <jessica2003mail@gmail.com>
2023-06-29 21:32:13 -0400

    Add config file

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express= require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes/router');
+const keys = require('./config/keys');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const dbURI = 'mongodb+srv://recipe-admin:2Hs3RSRp6rxuAanI@nodetut.lvubk6s.mongodb.net/recipe-database';
-mongoose.connect(dbURI)
+mongoose.connect(keys.mongo.dbURI)
     .then((result) => {
         app.listen(3000);
         console.log('connected to db');
